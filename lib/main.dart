e82e7cdef79d827d3d7722362b62bed74681f664
MasonMathias <mason.mathias04@gmail.com>
2025-10-01 18:37:33 -0400

    added the color picker in the appbar and the function to change the text color

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FadingTextAnimation(),
-    );
+    return MaterialApp(home: FadingTextAnimation());
   }
 }
 
@@ -21,11 +19,41 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  Color textColor = Colors.black;
 
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
     });
+  }
+
+  void _pickColor() {
+    Color temp = textColor;
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Pick a color'),
+        content: SingleChildScrollView(
+          child: ColorPicker(
+            pickerColor: temp,
+            onColorChanged: (c) => temp = c,
+            labelTypes: const [],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context), 
+            child: const Text('Cancel')),
+          TextButton(
+            onPressed: () {
+              setState(() => textColor = temp);
+              Navigator.pop(context);
+            },
+            child: const Text('Use'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -34,15 +62,19 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       appBar: AppBar(
         title: Text('Fading Text Animation'),
 
+        actions: [
+          IconButton(
+            tooltip: 'Pick color',
+            icon: const Icon(Icons.color_lens),
+            onPressed: _pickColor,
+          ),
+        ],
       ),
       body: Center(
         child: AnimatedOpacity(
           opacity: _isVisible ? 1.0 : 0.0,
           duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
+          child: Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
         ),
       ),
       floatingActionButton: FloatingActionButton(
